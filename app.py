2503065a85c15b25500e216c703a1c1f5a68a108
wereba <samuelwereba@gmail.com>
2023-02-07 15:46:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import dash
 import dash_core_components as dcc
-import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
 import dash_html_components as html
-from dash.dependencies import Input, Output
-import plotly.express as px
-
 import pandas as pd
+import plotly.express as px
+from azure.storage.blob import ContainerClient, BlobClient
+import io
+
+container_name = "gsr"
+blob_name = "Twatter.csv"
+account_url = "https://sam123.blob.core.windows.net"
+
+container_client = ContainerClient(account_url=account_url, container_name=container_name)
+blob_client = BlobClient(account_url=account_url, container_name=container_name, blob_name=blob_name)
+
+blob_data = blob_client.download_blob().readall().decode('utf-8')
+df = pd.read_csv(io.StringIO(blob_data))
+app = dash.Dash()
 
 
-
-dash_app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-app = dash_app.server
-
-dash_app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider')
+app.layout = html.Div([
+    dcc.Graph(
+        id='graph',
+        figure= px.Scatter(df, x='Date', y='Open'))
 ])
 
-
 if __name__ == '__main__':
-    dash_app.run_server(debug=True)
+    app.run_server(debug=True)
